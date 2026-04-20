c90cca3c51f54b88e93022af2e25738f8f547a7a
Echo8485 <steve1921@kau.kr>
2026-04-20 15:01:45 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -1,1 +1,31 @@
-ㅁ=1
+#include <stdio.h>
+
+int main() {
+    int answer;
+    int input;
+    int count = 0;
+
+    // 정답 입력
+    printf("정답 입력 : ");
+    scanf("%d", &answer);
+
+    do {
+        scanf("%d", &input);
+        count++;
+
+        if (input > answer) {
+            printf("%d>? ↦ %d 보다 낮습니다\n", input, input);
+        }
+        else if (input < answer) {
+            printf("%d<? ↦ %d 보다 높습니다\n", input, input);
+        }
+        else {
+            printf("%d==? ↦ %d 정답입니다\n", input, input);
+        }
+
+    } while (input != answer);
+
+    printf("%d ↦ 시도횟수는 %d회\n", answer, count);
+
+    return 0;
+}
